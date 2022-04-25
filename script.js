753e54f83e92c9ed96a526f00c74b05c603bb304
Karan Chandekar <karanchandekar431@gmail.com>
2022-04-25 15:25:25 +0530

    function to get random number

--- script.js
+++ script.js
@@ -68,3 +68,15 @@ function generatePassword(lower, upper, number, symbol, length) {
 
   return finalPassword;
 }
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
