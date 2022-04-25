a34b5d6470b9ef8f63de20b495b5cccc6a62930c
Karan Chandekar <karanchandekar431@gmail.com>
2022-04-25 15:25:59 +0530

    function to get random symbol

--- script.js
+++ script.js
@@ -80,3 +80,8 @@ function getRandomUpper() {
 function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
+
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
