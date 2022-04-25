1f54d7c46cb130cccf199a8e0deaddb29ce0553d
Karan Chandekar <karanchandekar431@gmail.com>
2022-04-25 15:21:26 +0530

    add click event to clipboard

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ const randomFunc = {
   number: getRandomNumber,
   symbol: getRandomSymbol,
 };
+
+clipboardEl.addEventListener("click", () => {
+  const textarea = document.createElement("textarea");
+  const password = resultEl.innerText;
+
+  if (!password) {
+    return;
+  }
+
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+  alert("Password copied to clipboard!");
+});
